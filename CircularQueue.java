f684fbac1a54ec8aadfc47e2ee1038ff86b8e480
McKay Hunter <155475147+mac8414@users.noreply.github.com>
2024-05-30 15:16:16 -0600

    Update CircularQueue.java

--- CircularQueue.java
+++ CircularQueue.java
@@ -34,10 +34,33 @@ public class CircularQueue<Item> implements Iterable<Item> {
       return SIZE;
    }
    public void enqueue(Item item){
-      
+      if (isFull()) {
+      System.out.println("Queue is full");
+    } else {
+      if (front == -1)
+        front = 0;
+      rear = (rear + 1) % SIZE;
+      items[rear] = element;
+      System.out.println("Inserted " + element);
+    }
    }
+   
    public Item dequeue(){
-
+         int element;
+    if (isEmpty()) {
+      System.out.println("Queue is empty");
+      return (-1);
+    } else {
+      element = items[front];
+      if (front == rear) {
+        front = -1;
+        rear = -1;
+      } /* Q has only one element, so we reset the queue after deleting it. */
+      else {
+        front = (front + 1) % SIZE;
+      }
+      return (element);
+    }
    }
    public Item peek(){
 
