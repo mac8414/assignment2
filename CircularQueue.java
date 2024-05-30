2ee01dec594b880f755ccdac20ed8a956b594d5f
McKay Hunter <155475147+mac8414@users.noreply.github.com>
2024-05-30 15:06:27 -0600

    Update CircularQueue.java

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,5 +1,39 @@
 package a02;
 
-public class CircularQueue {
-
+public class CircularQueue<Item> implements Iterable<Item> {
+   private int SIZE; // Size of Circular Queue
+   private int front, rear;
+   private int capacity;
+   private Item[] items;
+   
+   
+   CircularQueue(int capacity){
+       this.capacity = capacity;
+       this.SIZE = 0;
+       this.front = -1;
+       this.rear = -1;
+       this.items = (Item[]) new Object[capacity]; 
+   }  
+   public boolean isFull(){
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    if (front == rear + 1) {
+      return true;
+    }
+    return false;
+  }
+     
+   public boolean isEmpty(){
+    if (front == -1)
+      return true;
+    else
+      return false;
+  }
+   public int size(){
+   }
+   public void enqueue(Item item)
+   public Item dequeue()
+   public Item peek()
+   public String toString()
 }
