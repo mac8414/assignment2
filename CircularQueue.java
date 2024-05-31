3aa67d02fc7f2008c4d15d6c9ce3928ed972bf2c
RasimC26 <157428566+RasimC26@users.noreply.github.com>
2024-05-31 13:07:02 -0600

    Update CircularQueue.java

--- CircularQueue.java
+++ CircularQueue.java
@@ -127,4 +127,78 @@ public class CircularQueue<Item> implements Iterable<Item> {
             return item;
         }
 	}
+
+	public static void main(String[] args) {
+	
+	
+	 CircularQueue<String> q1 = new CircularQueue<>(3);
+	
+	
+	
+	 // isEmpty and isFull on an empty queue
+     System.out.println("Is queue empty? " + q1.isEmpty()); // true
+     
+     System.out.println("Is queue full? " + q1.isFull());   // false
+     
+     System.out.println("Size: " + q1.size());              // 0
+		
+		
+     //  enqueue testing
+     
+     q1.enqueue("1");
+     
+     System.out.println(q1.toString()); // 1
+     
+     q1.enqueue("2");
+     
+     System.out.println(q1.toString()); // 1 2
+     
+     q1.enqueue("3");
+     
+     System.out.println(q1.toString()); // 1 2 3
+     
+     
+     // isFull after enqueue
+     
+     System.out.println("Is the queue full? " + q1.isFull()); // true
+ 	
+ 	
+     // enqueue on a full queue
+     
+     q1.enqueue("4"); // Queue is full
+     
+     
+     
+     
+     // Dequeue
+     
+     System.out.println("Dequeued: " + q1.dequeue()); // Dequeued: 1
+     
+     System.out.println(q1.toString());              // 2 3
+     
+     System.out.println("Dequeued: " + q1.dequeue()); //Dequeued: 2
+     
+     System.out.println(q1.toString());              // 3
+     
+     // size after dequeue
+     System.out.println("Size: " + q1.size()); // Size: 1
+     
+     // Enqueue more items
+     q1.enqueue("4");
+     System.out.println(q1.toString()); // 3 4
+     q1.enqueue("5");
+     System.out.println(q1.toString()); // 3 4 5
+     
+     // peek
+     System.out.println("Peek: " + q1.peek()); // Peek: 3
+     
+     // remaining items
+     System.out.println("Dequeued: " + q1.dequeue()); // Dequeued: 3
+     System.out.println("Dequeued: " + q1.dequeue()); // Dequeued: 4 
+     System.out.println("Dequeued: " + q1.dequeue()); // Dequeued:  5
+     
+     // isEmpty after dequeue
+     System.out.println("Is queue empty? " + q1.isEmpty()); // "Is queue empty? true
+ 	
+	}
 }
